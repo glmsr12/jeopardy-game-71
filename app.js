25392ad4f07faa5233ccbd95543f47d279530296
glmsr12 <gulumsercakmak@hotmail.com>
2023-03-05 20:58:39 -0800

    "update file"

--- app.js
+++ app.js
@@ -11,10 +11,41 @@ const jeopardyCategories = [
         correct: 'Paris',
         level: 'easy',
       },
+      {
+        question: 'What is the capital of Ukraine?',
+        answers: ['Kyiv', 'Kiev'],
+        correct: 'Kiev',
+        level: 'easy',
+      },
+      {
+        question: 'What is the capital of Spain?',
+        answers: ['Madrid'],
+        correct: 'Madrid',
+        level: 'easy',
+      },
     ],
   },
   {
     genre: 'LANGUAGES',
-    questions: [],
+    questions: [
+      {
+        question: 'What language do Ukrainians speak?',
+        answers: ['Russian', 'Belarusian'],
+        correct: 'Russian and Belarusian',
+        level: 'easy',
+      },
+      {
+        question: 'What language do Ukrainians speak?',
+        answers: ['Russian', 'Belarusian'],
+        correct: 'Russian and Belarusian',
+        level: 'easy',
+      },
+      {
+        question: 'What language do Ukrainians speak?',
+        answers: ['Russian', 'Belarusian'],
+        correct: 'Russian and Belarusian',
+        level: 'easy',
+      },
+    ],
   },
 ];
