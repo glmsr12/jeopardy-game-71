3b25d8296462ece0e75553dff8b9f929700fead3
glmsr12 <gulumsercakmak@hotmail.com>
2023-03-06 10:45:56 -0800

    "questions completed"

--- app.js
+++ app.js
@@ -3,48 +3,94 @@ const score = document.getElementById('score');
 
 const jeopardyCategories = [
   {
-    genre: 'CAPITALS',
+    genre: 'WHO',
     questions: [
       {
-        question: 'What is the capital of France?',
-        answers: ['Paris'],
-        correct: 'Paris',
+        question: 'Who wrote the Harry Potter Books?',
+        answers: ['J.K. Rowling'],
+        correct: 'J.K. Rowling',
         level: 'easy',
       },
       {
-        question: 'What is the capital of Ukraine?',
-        answers: ['Kyiv', 'Kiev'],
-        correct: 'Kiev',
-        level: 'easy',
+        question: 'Who was born on Krypton?',
+        answers: ['Superman'],
+        correct: 'Superman',
+        level: 'medium',
       },
       {
-        question: 'What is the capital of Spain?',
-        answers: ['Madrid'],
-        correct: 'Madrid',
-        level: 'easy',
+        question: 'Who designed the Eiffel Tower?',
+        answers: ['Gustave Eiffel'],
+        correct: 'Gustave Eiffel',
+        level: 'hard',
       },
     ],
   },
   {
-    genre: 'LANGUAGES',
+    genre: 'WHERE',
     questions: [
       {
-        question: 'What language do Ukrainians speak?',
-        answers: ['Russian', 'Belarusian'],
-        correct: 'Russian and Belarusian',
+        question: 'Where is Central Park?',
+        answers: ['New York, Manhattan'],
+        correct: 'New York',
         level: 'easy',
       },
       {
-        question: 'What language do Ukrainians speak?',
-        answers: ['Russian', 'Belarusian'],
-        correct: 'Russian and Belarusian',
+        question: 'Where can you visit the Great Wall of China?',
+        answers: ['Beijing, China'],
+        correct: 'China',
+        level: 'medium',
+      },
+      {
+        question: 'Where is Colosseum?',
+        answers: ['Rome, Italy'],
+        correct: 'Italy',
+        level: 'hard',
+      },
+    ],
+  },
+  {
+    genre: 'WHEN',
+    questions: [
+      {
+        question: 'When was the planet Pluto discovered?',
+        answers: ['February 18, 1930'],
+        correct: '1930',
         level: 'easy',
       },
       {
-        question: 'What language do Ukrainians speak?',
-        answers: ['Russian', 'Belarusian'],
-        correct: 'Russian and Belarusian',
+        question: 'When was the first computer intervened?',
+        answers: ['1901, 1900, 20th century'],
+        correct: '1901',
+        level: 'medium',
+      },
+      {
+        question: 'When was the first mobile phone used?',
+        answers: ['April 3, 1973'],
+        correct: '1973',
+        level: 'hard',
+      },
+    ],
+  },
+  {
+    genre: 'WHAT',
+    questions: [
+      {
+        question: 'What is the most popular tourist destination in the world?',
+        answers: ['France, Spain, United State, Italy, China'],
+        correct: 'France, Spain, United State, Italy, China',
         level: 'easy',
+      },
+      {
+        question: 'What do koalas eat?',
+        answers: ['Eucalyptus leaves,brush box,paperback,bloodwood trees '],
+        correct: 'Eucalyptus leaves,brush box,paperback,bloodwood trees ',
+        level: 'medium',
+      },
+      {
+        question: 'What is the capital of United States?',
+        answers: ['Washington D.C.'],
+        correct: 'Washington D.C.',
+        level: 'hard',
       },
     ],
   },
